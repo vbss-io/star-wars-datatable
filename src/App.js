3a0c87a2b0740f410798c032e1d40fd7e63dcfe8
vitorbss12 <vitor_bss10@hotmail.com>
2022-06-10 15:43:51 -0300

    Requisito 1 - Atualizando tabela com dados da API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import MyContext from './helpers/MyContext';
+import Table from './components/Table';
 import './App.css';
 
+function useFetchApi(defaultValue) {
+  const [data, setData] = useState(defaultValue);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+      .then((response) => response.json())
+      .then((dataAPI) => {
+        setData(dataAPI.results);
+        setLoading(false);
+      });
+  }, []);
+  return [data, loading];
+}
+
 function App() {
+  const [data, loading] = useFetchApi([]);
   return (
-    <span>Criando PR pra ganhar a caneca</span>
+    <MyContext.Provider value={ data }>
+      {loading
+        ? <span>Loading...</span>
+        : <Table />}
+    </MyContext.Provider>
   );
 }
 
---
+++ src/components/Table.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import MyContext from '../helpers/MyContext';
+import '../style/Table.css';
+
+function Table() {
+  return (
+    <table className="planets">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation Period</th>
+          <th>Orbital Period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface Water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>Url</th>
+        </tr>
+      </thead>
+      <tbody>
+        <MyContext.Consumer>
+          {(context) => (
+            context.map((item) => (
+              <tr key={ item.name }>
+                <td>{ item.name }</td>
+                <td>{ item.rotation_period }</td>
+                <td>{ item.orbital_period }</td>
+                <td>{ item.diameter }</td>
+                <td>{ item.climate }</td>
+                <td>{ item.gravity }</td>
+                <td>{ item.terrain }</td>
+                <td>{ item.surface_water }</td>
+                <td>{ item.population }</td>
+                <td>{ item.films }</td>
+                <td>{ item.created }</td>
+                <td>{ item.edited }</td>
+                <td>{ item.url }</td>
+              </tr>
+            )))}
+        </MyContext.Consumer>
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
